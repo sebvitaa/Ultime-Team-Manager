8a6228ef90bd2150a48037ccc2d434768dd5e59a
sebvitaa <seramirezo@udd.cl>
2026-07-13 15:18:09 -0400

    Crear dependencias

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:contador_app/presentation/screens/counter/counter_screen.dart';
-
-void main() => runApp(const MyApp());
+void main() => runApp (const ProviderScope(child: MyApp()));
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
